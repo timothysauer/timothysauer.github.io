13fed6d347dd6835563c1f1837222f7990557e26
timothysauer <122749884+timothysauer@users.noreply.github.com>
2025-09-23 09:21:04 -0400

    Update burgers.py

--- code/burgers.py
+++ code/burgers.py
@@ -19,9 +19,6 @@ def burgers(xl, xr, tb, te, M, N):
     w1 = w[:, 0].copy()
     for j in range(n):
         for it in range(3):  # Newton iteration, 3 steps
-            #DF1 = np.zeros((m, m))
-            #DF2 = np.zeros((m, m))
-
             DF1 = np.diag(1 + 2 * sigma * np.ones(m)) + np.diag(-sigma * np.ones(m - 1), 1) + np.diag(-sigma * np.ones(m - 1), -1)
             DF2 = np.zeros((m, m))
             DF2[1:m - 1, 1:m-1] = np.diag(k * w1[2:m] / (2 * h)) - np.diag(k * w1[0:m - 2] / (2 * h))
@@ -29,22 +26,14 @@ def burgers(xl, xr, tb, te, M, N):
             DF2[0:m-1,0:m-1] -= np.diag(k * w1[1:m - 1] / (2 * h), -1)
             DF = DF1 + DF2
             F = -w[:, j] + (DF1 + DF2 / 2) @ w1  # Using Lemma 8.11
-
-            # Dirichlet conditions for DF
-            DF[0, :] = np.zeros(m)
+            DF[0, :] = np.zeros(m)  # Dirichlet conditions for DF
             DF[0, 0] = 1
             DF[m-1, :] = np.zeros(m)
             DF[m-1, m-1] = 1
-            
-            # Dirichlet conditions for F
-            F[0] = w1[0] - l(j)
+            F[0] = w1[0] - l(j)  # Dirichlet conditions for F
             F[m-1] = w1[m-1] - r(j)
-
-            # Solve the linear system
             w1 = w1 - np.linalg.solve(DF, F)
-
-        # Update the solution for the next time step
-        w[:, j + 1] = w1
+        w[:, j + 1] = w1  # Update the solution for the next time step
 
     # Prepare data for visualization
     x = np.linspace(xl, xr, m)
