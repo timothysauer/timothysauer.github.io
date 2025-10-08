1fb36896f76af3275d54333c172463cbbd629dbe
timothysauer <122749884+timothysauer@users.noreply.github.com>
2025-10-08 13:49:30 -0400

    update

--- code/crank.py
+++ code/crank.py
@@ -2,23 +2,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def crank(xl, xr, yb, yt, M, N):
-    f = lambda x: np.sin(2 * np.pi * x) ** 2
-    l = lambda t: 0 * t  # Left boundary condition
-    r = lambda t: 0 * t  # Right boundary condition
+    """ Program 8.4 Crank-Nicolson Method with
+            Dirichlet boundary conditions
+        Input:  [xl,xr] space interval
+                [yb,yt] time interval
+                M number of space steps
+                N number of time steps
+        Output: w solution """
+    def f(x): return np.sin(2*np.pi*x)**2  # Initial condition function
+    def l(t): return 0*t  # Left boundary condition
+    def r(t): return 0*t  # Right boundary condition
     D = 1  # diffusion coefficient
-    h = (xr - xl) / M  # space step size
-    k = (yt - yb) / N  # time step size
-    sigma = D * k / (h ** 2)
+    h = (xr - xl)/M  # space step size
+    k = (yt - yb)/N  # time step size
+    sigma = D*k/(h**2)
     m = M - 1  # Number of interior nodes
     n = N  # Number of time steps
     # Define tridiagonal matrices a and b
-    a = np.diag(2 + 2 * sigma * np.ones(m)) \
-        + np.diag(-sigma * np.ones(m - 1), 1) \
-        + np.diag(-sigma * np.ones(m - 1), -1)
-    b = np.diag(2 - 2 * sigma * np.ones(m)) \
-        + np.diag(sigma * np.ones(m - 1), 1) \
-        + np.diag(sigma * np.ones(m - 1), -1)
-    # Set up boundary conditions for each time step
+    a = np.diag(2+2*sigma*np.ones(m))+np.diag(-sigma*np.ones(m-1),1)
+    a +=  np.diag(-sigma * np.ones(m - 1), -1)
+    b = np.diag(2 - 2*sigma*np.ones(m))+ np.diag(sigma*np.ones(m-1),1) 
+    b +=  np.diag(sigma * np.ones(m - 1), -1)
     lside = l(yb + np.arange(n + 1) * k)  # Left boundary values
     rside = r(yb + np.arange(n + 1) * k)  # Right boundary values
     w = np.zeros((m + 2, n + 1))  # Initial conditions
@@ -28,9 +32,7 @@ def crank(xl, xr, yb, yt, M, N):
         sides[0] = lside[j] + lside[j + 1]
         sides[1:m - 1] = 0  # Middle part remains zero
         sides[m - 1] = rside[j] + rside[j + 1]
-        # Solve the system and store in w
-        w[1:m + 1, j + 1] = np.linalg.solve(a, b @ w[1:m + 1, j] \
-            + sigma * sides)
+        w[1:m+1, j+1] = np.linalg.solve(a,b@w[1:m+1,j]+sigma*sides)
     w = np.vstack((lside, w, rside))
     x = xl + np.arange(0, M + 3) * h    # grids for plotting
     t = yb + np.arange(0, N + 1) * k
@@ -40,7 +42,7 @@ def crank(xl, xr, yb, yt, M, N):
     ax.plot_surface(X, T, w.T, cmap='viridis', vmin=-1.,vmax=1.)
     plt.xlabel('x')
     plt.ylabel('t')
-    plt.title('Crank-Nicolson Method')
+    plt.title('Crank-Nicolson Method for heat equation')
     plt.xlim(xl, xr)
     plt.ylim(yb, yt)
     ax.set_zlim(-1.,1.)
--- code/heatfd.py
+++ code/heatfd.py
@@ -2,33 +2,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def heatfd(xl, xr, yb, yt, M, N):
-    f = lambda x: np.sin(2*np.pi*x)**2  # Initial condition function
-    l = lambda t: 0*t  # Left boundary condition
-    r = lambda t: 0*t  # Right boundary condition
+    """ Program 8.1 Forward Difference Method for heat equation
+        Input:  [xl,xr] space interval
+                [yb,yt] time interval
+                M number of space steps
+                N number of time steps
+        Output: w solution """
+    def f(x): return np.sin(2*np.pi*x)**2  # Initial condition function
+    def l(t): return 0*t  # Left boundary condition
+    def r(t): return 0*t  # Right boundary condition
     D = 1  # Diffusion coefficient
     h = (xr - xl)/M  # Space step size
     k = (yt - yb)/N  # Time step size
     m = M - 1  # Number of spatial steps
     n = N  # Number of time steps
     sigma = D*k/(h**2)  # Stability parameter
-    # Define matrix A
-    a = np.diag(1-2*sigma*np.ones(m))+np.diag(sigma*np.ones(m-1),1)+np.diag(sigma*np.ones(m-1),-1)
-    # Set boundary conditions
+    a = np.diag(1-2*sigma*np.ones(m))   # define coefficient matrix a
+    a += np.diag(sigma*np.ones(m-1),1)+np.diag(sigma*np.ones(m-1),-1)
     lside = l(yb + np.arange(n+1)*k)  # Left boundary values
     rside = r(yb + np.arange(n+1)*k)  # Right boundary values
-    # Initialize solution matrix
-    w = np.zeros((m, n + 1))
+    w = np.zeros((m, n + 1))   # Initialize solution matrix
     w[:,0] = f(xl + (1+np.arange(m))*h)  # Initial conditions
-    # Time-stepping
     for j in range(n):
-        w[:,j+1] = a@w[:,j] + sigma*np.concatenate(([lside[j]],np.zeros(m-2),[rside[j]]))
-    # Attach boundary conditions
-    w = np.vstack((lside, w, rside))
-    # Prepare for plotting
+        w[:,j+1]=a@w[:,j]+sigma*np.concatenate(([lside[j]],np.zeros(m-2),[rside[j]]))
+    w = np.vstack((lside, w, rside))   # Attach boundary conditions
     x = np.linspace(xl, xr, m + 2)
     t = np.linspace(yb, yt, n + 1)
-    # 3-D Plot of the solution
-    X, T = np.meshgrid(x, t)
+    X, T = np.meshgrid(x, t)   # 3-D Plot of the solution
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.plot_surface(X, T, w.T, cmap='viridis', vmin=-1.,vmax=1.)
@@ -41,7 +41,6 @@ def heatfd(xl, xr, yb, yt, M, N):
     ax.set_zlim(-1.,1.)
     ax.view_init(elev=30, azim=-30)  # Set the view angle
     plt.show()
-
     return w
 
 # Example usage
