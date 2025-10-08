71169abd811a705f1c36836a718f7b2ee86a46d4
timothysauer <122749884+timothysauer@users.noreply.github.com>
2025-10-08 12:00:45 -0400

    update

--- code/predcorr.py
+++ code/predcorr.py
@@ -2,6 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def predcorr(inter, ic, n, s):
+    """ Program 6.8 Adams-Bashforth-Mounton second-order p-c
+        Input:  inter time interval
+                ic initial conditions
+                n number of steps
+                s number of multisteps for explicit method
+        Output: t time points
+                y solution """
     h = (inter[1] - inter[0]) / n  # Step size
     y = np.zeros((n + 1, len(ic)))  # Initialize solution array
     t = np.zeros(n + 1)             # Initialize time array
@@ -22,30 +29,24 @@ def predcorr(inter, ic, n, s):
     plt.plot(t, y[:, 0])
     plt.xlabel('Time')
     plt.ylabel('y')
-    plt.title('Predicted-Corrected Method for ODEs')
+    plt.title('Predictor-Corrector Method for ODEs')
     plt.grid()
     plt.show()
-        
     return t, y
 
-def trapstep(t, x, h):
-    """ One step of the Trapezoid Method """
+def trapstep(t, x, h):  # One step of the Trapezoid Method
     z1 = ydot(t, x)
-    g = x + h * z1
+    g = x + h*z1
     z2 = ydot(t + h, g)
-    return x + h * (z1 + z2) / 2
+    return x + h*(z1 + z2)/2
 
-def ab2step(t, i, y, f, h):
-    """ One step of the Adams-Bashforth 2-step method """
-    return y[i, :] + h * (3 * f - ydot(t, y[i - 1, :])) / 2
+def ab2step(t,i,y,f,h):  # One step of AB 2-step method
+    return y[i, :] + h*(3*f - ydot(t, y[i-1, :]))/2
 
-def am1step(t, i, y, f_i, f_i_next, h):
-    """ One step of the Adams-Moulton 1-step method """
-    return y[i, :] + h * (f_i_next + f_i) / 2
+def am1step(t,i,y,f_i,f_i_next,h): # One step of the AM 1-step method 
+    return y[i, :] + h*(f_i_next + f_i)/2
 
-def ydot(t, y):
-    """ Derivative function for the initial value problem (IVP) """
-    return t * y + t**3
+def ydot(t, y):  return t * y + t**3
 
 # Example usage
 t, y = predcorr([0, 1], [1], 20, 2)
