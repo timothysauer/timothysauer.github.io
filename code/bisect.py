77654359fb107fa15172f28b5aa9f0325c789503
timothysauer <122749884+timothysauer@users.noreply.github.com>
2025-10-12 12:02:46 -0400

    yd

--- code/bisect.py
+++ code/bisect.py
@@ -24,3 +24,7 @@ def bisect(f, a, b, tol):
             fa = fc
     xc = (a + b) / 2  # New midpoint is the best estimate
     return xc
+
+# Example usage:
+def f(x): return x**3 + x - 1
+xc = bisect(f, 0, 1, 0.5e-4)
