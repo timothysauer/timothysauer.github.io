874f4c79fdc568de614a975bf33cece177b76ab1
timothysauer <122749884+timothysauer@users.noreply.github.com>
2025-10-13 10:59:22 -0400

    Update fpi.py

--- code/fpi.py
+++ code/fpi.py
@@ -6,14 +6,14 @@ def fpi(g, x0, k):
             k number of steps
     Output: xc approximate fixed point
     """
-    x = [0] * (k + 1)
+    x = [0]*(k+1)
     x[0] = x0
     for i in range(k):
-        x[i + 1] = g(x[i]) 
+        x[i+1] = g(x[i]) 
     xc = x[k]  
     return xc
 
 # Example usage
-# def g(x): return (x + 2) / 3
+# def g(x): return (x+2)/3
 # xc = fpi(g, 0., 10)
 
