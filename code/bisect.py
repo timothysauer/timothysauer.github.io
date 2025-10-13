196677505247e590e1d94949af624128f8151e9e
timothysauer <122749884+timothysauer@users.noreply.github.com>
2025-10-13 14:10:34 -0400

    update

--- code/bisect.py
+++ code/bisect.py
@@ -26,5 +26,5 @@ def bisect(f, a, b, tol):
     return xc
 
 # Example usage:
-def f(x): return x**3 + x - 1
-xc = bisect(f, 0, 1, 0.5e-4)
+#def f(x): return x**3 + x - 1
+#xc = bisect(f, 0, 1, 0.5e-4)
