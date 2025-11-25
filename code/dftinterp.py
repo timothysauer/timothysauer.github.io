b8031b631d29293f8947737e5d397e85c1e57e5e
timothysauer <122749884+timothysauer@users.noreply.github.com>
2025-11-25 16:15:17 -0500

    Update dftinterp.py

--- code/dftinterp.py
+++ code/dftinterp.py
@@ -5,8 +5,7 @@ def dftinterp(inter, x, p):
     """ Program 10.1 Fourier Interpolation
         Input:  [c,d] interval
                 x data points
-                p length of interpolant (even number >= len(x))
-    """
+                p length of interpolant (even number >= len(x))"""
     c, d = inter
     n = x.shape[0]
     t = c + (d - c)*np.arange(n)/n  # Original data points
@@ -17,14 +16,14 @@ def dftinterp(inter, x, p):
     yp[:n//2 + 1] = y[:n//2 + 1]  # Lower half frequencies
     yp[p - n//2 + 1:p] = y[n//2 + 1:n]  # Upper half frequencies
     xp = np.real(np.fft.ifft(yp))*(p/n) # Invert FFT to recover data
-    plt.plot(t, x, 'o', label='Data Points') # Plot original data 
-    plt.plot(tp, xp, label='Interpolant') # Plot interpolant
-    plt.xlabel('Time')
-    plt.ylabel('Value')
-    plt.title('Fourier Interpolation')
-    plt.legend()
-    plt.show()
-    return xp
+    return tp, xp
 
 # Example usage
-xp = dftinterp([0,2*np.pi],np.sin(np.arange(16)*2*np.pi/16),64)
+tp, xp = dftinterp([0,2*np.pi],np.sin(np.arange(16)*2*np.pi/16),64)
+plt.plot(t, x, 'o', label='Data Points') # Plot original data 
+plt.plot(tp, xp, label='Interpolant') # Plot interpolant
+plt.xlabel('Time')
+plt.ylabel('Value')
+plt.title('Fourier Interpolation')
+plt.legend()
+plt.show()
