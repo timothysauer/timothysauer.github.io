28072eb8cdbf4b316609225a1ae0756a35b7c2a4
timsauer <timsauer@users.noreply.github.com>
2026-01-13 10:42:02 -0800

    up

--- code/euler.py
+++ code/euler.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def euler(ydot,inter, y0, n):
+def euler(f,inter, y0, n):
     """Program 6.1 Euler's Method
-    Solve the IVP y' = ydot(t,y)
-    Input:  ydot right-hand side function of ODE
+    Solve the IVP y' = f(t,y)
+    Input:  f right-hand side function of ODE
             inter time interval of solution
             y0 initial value
             n number of time steps
@@ -17,16 +17,16 @@ def euler(ydot,inter, y0, n):
     h = (inter[1] - inter[0])/n  # Step size
     for i in range(n):
         t[i + 1] = t[i] + h
-        y[i + 1] = eulerstep(ydot, t[i], y[i], h)
-    plt.plot(t, y)
-    plt.xlabel('Time')
-    plt.ylabel('Solution')
-    plt.title('Euler Method')  
-    plt.show()
+        y[i + 1] = eulerstep(f, t[i], y[i], h)
     return t, y
 
-def eulerstep(ydot, t, y, h): return y + h*ydot(t, y)
+def eulerstep(f, t, y, h): return y + h*f(t, y)
 
 # Example usage
-#def ydot(t, y): return t*y + t**3
-#y = euler(ydot,[0, 1], 1., 10)
+def f(t, y): return t*y + t**3
+t, y = euler(f,[0, 1], 1., 10)
+plt.plot(t, y)
+plt.xlabel('Time')
+plt.ylabel('Solution')
+plt.title('Euler Method')  
+plt.show()
--- code/euler2.py
+++ code/euler2.py
@@ -1,10 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def euler2(ydot,inter, y0, n):
+def euler2(f,inter, y0, n):
     """Program 6.2 Euler's Method, vector version
-    Solve the IVP y' = ydot(t,y)
-    Input:  ydot right-hand side function of ODE
+    Solve the IVP y' = f(t,y)
+    Input:  f right-hand side function of ODE
             inter time interval of solution
             y0 initial vector
             n number of time steps
@@ -17,23 +17,22 @@ def euler2(ydot,inter, y0, n):
     h = (inter[1] - inter[0]) / n  # Step size
     for i in range(n):
         t[i + 1] = t[i] + h
-        y[i + 1, :] = eulerstep(t[i], y[i, :], h)
-    plt.plot(t, y[:, 0], label='y0')
-    plt.plot(t, y[:, 1], label='y1')
-    plt.xlabel('Time')
-    plt.ylabel('Solution')
-    plt.title('Euler Method')
-    plt.legend()
-    plt.show()
+        y[i + 1, :] = eulerstep(f, t[i], y[i, :], h)
     return t, y
 
-def eulerstep(t, y, h):
-    return y + h * ydot(t, y)
+def eulerstep(f, t, y, h): return y + h * f(t, y)
 
 # Example usage
-#def ydot(t, y):
-#    z = np.zeros(2)
-#    z[0] = y[1]**2 - 2 * y[0]
-#    z[1] = y[0] - y[1] - t * y[1]**2
-#    return z
-#y = euler2(ydot,[0, 1], [0, 1], 10)
+def f(t, y):
+    z = np.zeros(2)
+    z[0] = y[1]**2 - 2 * y[0]
+    z[1] = y[0] - y[1] - t * y[1]**2
+    return z
+t, y = euler2(f, [0, 1], [0, 1], 10)
+plt.plot(t, y[:, 0], label='y0')
+plt.plot(t, y[:, 1], label='y1')
+plt.xlabel('Time')
+plt.ylabel('Solution')
+plt.title('Euler Method')
+plt.legend()
+plt.show()
